b2b239ffa8eb65ef1ddc3f90767277b1ab00b914
BBML-DEV <pv.marques2010@hotmail.com>
2022-05-20 08:02:58 -0300

    projeto finalizado

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+const cards = document.querySelectorAll(".card");
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+function flipCard() {
+    if(lockBoard) return;
+    
+    this.classList.add("flip");
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMath();
+}
+
+function checkForMath() {
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disableCards();
+        return
+    }
+
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(()=>{
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        lockBoard = false;
+    }, 1500);
+}
+
+cards.forEach((card) => {
+    card.addEventListener("click", flipCard);
+});
